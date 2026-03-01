66186c0a1d55c7bd86d50c08861460f4760dec0d
Anandsalimath <mail2anand.as@gmail.com>
2026-03-01 17:55:26 +0530

    Refactor Streamlit app header and remove markdown

--- demo_lin_reg_fit.py
+++ demo_lin_reg_fit.py
@@ -6,9 +6,7 @@ with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Create the Streamlit web app
-st.header("🌟 Streamlit demo 🌟")
-st.markdown("### *Dedicated to the Legend: Shridhar Sir* 👨‍🏫✨")
-st.markdown("> *Where there's Shridhar, there's brilliance. Where there's brilliance, there's this app!* 💫")
+st.header("Streamlit demo")
 
 st.sidebar.header("This is a web app")
 
@@ -21,6 +19,3 @@ yhat_test = model.predict([[X_test]])
 st.write("b0 is", round(model.intercept_, 3))
 st.write("b1 is", round(model.coef_[0], 3))
 st.write("yhat test is", yhat_test)
-
-st.markdown("---")
-st.markdown("*Built with ❤️ and inspired by Shridhar Sir's guidance*")
